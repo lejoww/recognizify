4b2ed932341660da67131602d8ddb272c8943856
Ale Rodríguez <est.aedrz@gmail.com>
2019-11-01 17:07:59 -0500

    sentry installation

--- src/App.vue
+++ src/App.vue
@@ -22,11 +22,20 @@
 </style>
 <script>
 
-
+  import Vue from 'vue'
   import '@/assets/dist/css/bootstrap.css'
   import '@/assets/dist/js/bootstrap.js'
   import '@/assets/css/main.css'
   import '@/assets/scripts/firebase-init.js'
+
+  // sentry js 
+  import * as Sentry from '@sentry/browser'
+  import * as Integrations from '@sentry/integrations'
+
+  Sentry.init({
+    dsn: 'https://7b994449f84e45d59ebd974c5cdefdcb@sentry.io/1805378',
+    integrations: [new Integrations.Vue({Vue, attachProps: true})],
+  })
   
   export default {}
 
