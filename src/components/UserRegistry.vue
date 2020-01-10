1be12595b249612f9ab961154fb82b83c8a5550e
Ale Rodríguez <est.aedrz@gmail.com>
2020-01-10 13:22:27 -0500

    users module redesign #1

--- src/components/UserRegistry.vue
+++ src/components/UserRegistry.vue
@@ -11,15 +11,17 @@
               <th scope="col">Rol</th>
             </tr>
           </thead>
-          <tbody id="projectUsersField"></tbody>
+          <tbody id="projectUsersField">
+            <tr :key="user.name" v-for="user in users">
+              <td scope="col">
+                <img class="userProfilePhoto" :src="user.picture" width="25px">
+                {{user.name}}
+              </td>
+              <td scope="col">@{{user.user}}</td>
+              <td scope="col">{{user.role ? user.role : 'No tiene rol asignado'}}</td>
+            </tr>
+          </tbody>
         </table>
-      </div>
-
-      <div class="bannerNonUsers" id="bannerNonUsers">
-        <div>
-          <h3>Invita a más personas a tu proyecto</h3>
-          <p>Puedes agregar a más usuarios en el proyecto si conoces su nombre de usuario. Para añadir un miembro a tu proyecto, ya debe de estar registrado en la plataforma. Solo debes acceder a https://recognizify.netlify.com/@ y agregarle a esa dirección el nombre de usuario de la persona.</p>
-        </div>
       </div>
     </div>
 </template>
@@ -34,39 +36,34 @@
       return {
         search: '',
         UserNameInvited: '',
-        UserInvited: ''
+        UserInvited: '',
+        users: []
       }
     },
     mounted: function(){
-      this.getUsersOnThisProject()
+      firebase.firestore()
+      .collection('projects')
+      .doc(this.$route.params.projectId)
+      .collection('members')
+      .get()
+        .then(users => {
+          users.forEach(user => {
+            firebase.storage()
+            .ref(`/profile_photos/${user.id}`)
+            .getDownloadURL()
+            .then((url) => {
+              this.users.push({
+                name: user.data()['name'],
+                picture: url,
+                user: user.data()['user'],
+                role: user.data()['role']
+              })
+            })
+          })
+        })
     },
     mixins: [AddPoints],
     methods: {
-      getUsersOnThisProject: function(){
-        let projectCode = this.$router.history.current.params['projectId']
-        firebase.firestore().collection('projects').doc(projectCode).collection('members').get()
-          .then(users => {
-            console.log(users.docs.length)
-            if(users.docs.length >= 1){
-              let tableContainer = document.getElementById('usersListTable')
-              let usersTable = document.getElementById('projectUsersField')
-
-              tableContainer.style.display = 'block'
-              users.forEach(user => {
-                usersTable.insertAdjacentHTML('beforeend', `
-                  <tr>
-                    <td>${user.data()['name']}</td>
-                    <td>${user.data()['user'] ? '@' + user.data()['user'] : 'No tiene un usuario'}</td>
-                    <td>${user.data()['role'] ? user.data()['role'] : 'No tiene rol asignado'}</td>
-                  </tr>
-                `)
-              })
-            } if (users.docs.length == 0) {
-              let bannerNonUsers = document.getElementById('bannerNonUsers')
-              bannerNonUsers.style.display = 'block'
-            }
-          })
-      },
       proposeAnUserWithSearch: function(){
         firebase.firestore().collection('users').get()
           .then(users => {
