2b08eb7202abcfbd423599871b10eedd9b514ad9
Ale Rodríguez <est.aedrz@gmail.com>
2020-01-30 10:27:07 -0500

    feature: options menu on boards (outstanding)

--- src/components/modals/Features.vue
+++ src/components/modals/Features.vue
@@ -4,13 +4,12 @@
             <div class="modal-content">
                 <div class="modal-body" style="border: none">
                     <span class="subtitleOverline text-primary">Últimas novedades</span>
-                    <h4>Buscador de usuarios</h4><br>
+                    <h4>Destacar ideas en Boards</h4><br>
 
-                    <p>Ahora puedes buscar usuarios en el buscador del encabezado. 
-                    Escribe su nombre y podrás ver un listado de los posibles usuarios que buscas. </p>
+                    <p>En el  menú de opciones de las ideas de Boards puedes destacar ideas que puedan
+                        ser relevantes para tu proyecto. Pronto tendremos otras opciones para uno de los
+                        módulos más importantes de la familia. </p>
                     
-                    <p> Ten en cuenta que ahí
-                    solo puedes buscar perfiles que este configurados como públicos.</p>
                 </div>
                 <div class="modal-footer" style="border: none">
                     <button type="button" class="btn btn-primary btn-sm" data-dismiss="modal" @click="setModalChecked">Continuar</button>
@@ -32,15 +31,15 @@
 
     export default {
         mounted: function() {
-            if (localStorage.getItem('featuresModalChecked') != 'checked'){
+            // if (localStorage.getItem('featuresModalChecked') != 'checked'){
                 $("#featuresModal").modal("show");
-            }
+            // }
         },
-        methods: {
-            setModalChecked: function(){
-                localStorage.setItem('featuresModalChecked', 'checked')
-            }
-        }
+        // methods: {
+        //     setModalChecked: function(){
+        //         localStorage.setItem('featuresModalChecked', 'checked')
+        //     }
+        // }
     }
 
 </script>
